ea12294a7497bcf6064d4b963717c552e580f015
florianlft <125215761+florianlft@users.noreply.github.com>
2023-08-01 15:16:50 +0200

    1AOUT

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const items = document.querySelectorAll('.item');
 const cursorImage = document.querySelector('.cursorImage');
 
 items.forEach(item => {
-  const imageSrc = item.getAttribute('data-img');
+    const imageSrc = item.getAttribute('data-img');
 
-  item.addEventListener('mouseover', () => {
-    cursorImage.innerHTML = `<img src="${imageSrc}" alt="Image">`;
-    cursorImage.style.display = 'block';
-  });
+    item.addEventListener('mouseover', () => {
+        cursorImage.innerHTML = `<img src="${imageSrc}" alt="Image">`;
+        cursorImage.style.display = 'block';
+    });
 
-  item.addEventListener('mouseout', () => {
-    cursorImage.innerHTML = '';
-    cursorImage.style.display = 'none';
-  });
+    item.addEventListener('mouseout', () => {
+        cursorImage.innerHTML = '';
+        cursorImage.style.display = 'none';
+    });
 
-  item.addEventListener('mousemove', (event) => {
-    const offsetX = 20;
-    const offsetY = 20;
-    cursorImage.style.left = (event.pageX + offsetX) + 'px';
-    cursorImage.style.top = (event.pageY + offsetY) + 'px';
-  });
+    item.addEventListener('mousemove', (event) => {
+        const offsetX = 200;
+        const offsetY = 0;
+        cursorImage.style.left = (event.pageX + offsetX) + 'px';
+        cursorImage.style.top = (event.pageY + offsetY) + 'px';
+    });
 });
