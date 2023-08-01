7cace05bc9f76e7c5af1f239a2642ae65f18223d
florianlft <125215761+florianlft@users.noreply.github.com>
2023-08-01 12:25:56 +0200

    hoverimg2

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-const items = document.querySelectorAll('.item');
 const cursorImage = document.querySelector('.cursorImage');
 
-items.forEach(item => {
-  const imageSrc = item.getAttribute('data-img'); // Remplacez data-img-src par data-img
-
-  item.addEventListener('mouseover', () => {
+document.addEventListener('mousemove', (event) => {
+  const imageSrc = event.target.getAttribute('data-img');
+  if (imageSrc) {
     cursorImage.innerHTML = `<img src="${imageSrc}" alt="Image">`;
     cursorImage.style.display = 'block';
-  });
-
-  item.addEventListener('mouseout', () => {
+    cursorImage.style.left = (event.pageX - cursorImage.offsetWidth / 2) + 'px';
+    cursorImage.style.top = (event.pageY - cursorImage.offsetHeight / 2) + 'px';
+  } else {
     cursorImage.innerHTML = '';
     cursorImage.style.display = 'none';
-  });
-
-  item.addEventListener('mousemove', (event) => {
-    const offsetX = 20;
-    const offsetY = 20;
-    cursorImage.style.left = (event.pageX + offsetX) + 'px';
-    cursorImage.style.top = (event.pageY + offsetY) + 'px';
-  });
+  }
 });
