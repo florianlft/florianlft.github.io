eb6929d8ca4f682e3c009a3a0ab1e2b66702412e
florianlft <125215761+florianlft@users.noreply.github.com>
2023-08-01 12:57:02 +0200

    changement

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const items = document.querySelectorAll('.item');
-const cursorImage = document.querySelector('.cursorImage img');
+const cursorImage = document.querySelector('.cursorImage');
 
 items.forEach(item => {
   const imageSrc = item.getAttribute('data-img');
 
   item.addEventListener('mouseover', () => {
-    cursorImage.src = imageSrc; // Modifiez seulement la source de l'image
+    cursorImage.innerHTML = `<img src="${imageSrc}" alt="Image">`;
     cursorImage.style.display = 'block';
   });
 
   item.addEventListener('mouseout', () => {
+    cursorImage.innerHTML = '';
     cursorImage.style.display = 'none';
   });
 
   item.addEventListener('mousemove', (event) => {
-    const offsetX = -20; // Valeur négative pour déplacer l'image à gauche
-    const offsetY = -20; // Valeur négative pour déplacer l'image vers le haut
+    const offsetX = 20;
+    const offsetY = 20;
     cursorImage.style.left = (event.pageX + offsetX) + 'px';
     cursorImage.style.top = (event.pageY + offsetY) + 'px';
   });
